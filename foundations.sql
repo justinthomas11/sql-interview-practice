73b51611c1b8c2f882f6b918707edd5a1bae5e25
Justin <justinsthomas2020@gmail.com>
2026-03-19 15:01:02 +0530

    Day 2- Foundation queries complete

--- foundations.sql
+++ foundations.sql
@@ -24,3 +24,36 @@ select name, population, area from World where area>=300000 or population>=25000
 
 select name from Customer where referee_id!=2 or referee_id is null
 
+
+
+
+
+-- ** DAY 2**
+
+-- PROBLEM: Customers Who Never Order
+-- DIFFICULTY: Easy
+
+select c.name as "Customers" FROM Customers c 
+LEFT JOIN Orders o
+on c.id=o.customerId
+where o.customerId is null
+
+
+
+
+-- PROBLEM: Invalid Tweets
+-- DIFFICULTY: Easy
+
+select tweet_id from Tweets where LENGTH(content)>15
+
+
+
+-- PROBLEM: Article Views I
+
+select distinct author_id as "id" from Views WHERE
+author_id=viewer_id order by author_id ASC
+
+
+
+
+
